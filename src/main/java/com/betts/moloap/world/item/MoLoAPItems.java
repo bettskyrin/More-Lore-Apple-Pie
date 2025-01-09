ccb6460524e1b04a9ebfb18ceb4cf3ca95a6c164
Kyrin Betts <bettskyrin@gmail.com>
2025-01-09 17:13:35 -0600

    Added apple pie item

---
+++ src/main/java/com/betts/moloap/world/item/MoLoAPItems.java
@@ -0,0 +1,30 @@
+package com.betts.moloap.world.item;
+
+import com.betts.moloap.world.food.MoLoAPFoods;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+
+import static com.betts.moloap.MoreLoreApplePie.MOD_ID;
+import static net.minecraft.world.item.Items.registerItem;
+
+public class MoLoAPItems {
+    public static final Item APPLE_PIE = registerItem(itemId("apple_pie"), Item::new, new Item.Properties().food(MoLoAPFoods.APPLE_PIE));
+
+    private static ResourceKey<Item> itemId(String itemName) {
+        return ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(MOD_ID, itemName));
+    }
+
+    public static void initialize() {
+        ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.FOOD_AND_DRINKS).register(entries -> {
+            entries.addAfter(Items.PUMPKIN_PIE, MoLoAPItems.APPLE_PIE);
+        });
+    /* For Developer Use
+         LOGGER.info("Registering items...");
+     */
+    }
+}
