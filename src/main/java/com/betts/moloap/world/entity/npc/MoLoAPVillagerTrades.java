85d634be42a6ddb88126319397d81632e1531370
Kyrin Betts <bettskyrin@gmail.com>
2025-01-13 03:23:46 -0600

    Properly set trade XP

--- src/main/java/com/betts/moloap/world/entity/npc/MoLoAPVillagerTrades.java
+++ src/main/java/com/betts/moloap/world/entity/npc/MoLoAPVillagerTrades.java
@@ -12,7 +12,7 @@ public class MoLoAPVillagerTrades {
     public static void register() {
         TradeOfferHelper.registerVillagerOffers(VillagerProfession.FARMER, 2, factories -> {
             factories.add((entity, randomSource) -> new MerchantOffer(
-                    new ItemCost(Items.EMERALD), new ItemStack(MoLoAPItems.APPLE_PIE).copyWithCount(4), 4, 1, 0.05F)
+                    new ItemCost(Items.EMERALD), new ItemStack(MoLoAPItems.APPLE_PIE).copyWithCount(4), 4, 5, 0.05F)
             );
         });
     }
