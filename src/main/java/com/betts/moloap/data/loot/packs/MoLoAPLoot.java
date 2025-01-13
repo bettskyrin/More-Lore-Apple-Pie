52607e9c70c2b6df7baa46a303b66df6f303dfd8
Kyrin Betts <bettskyrin@gmail.com>
2025-01-12 21:23:48 -0600

    Refactor

--- src/main/java/com/betts/moloap/data/loot/packs/MoLoAPLoot.java
+++ src/main/java/com/betts/moloap/data/loot/packs/MoLoAPLoot.java
@@ -11,14 +11,14 @@ import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
 
 public class MoLoAPLoot {
     public static void modify() {
-        LootTableEvents.MODIFY.register((key, tableBuilder, source, registries) -> {
-            if (BuiltInLootTables.VILLAGE_PLAINS_HOUSE.equals(key) && source.isBuiltin()) {
+        LootTableEvents.MODIFY.register((resourceKey, tableBuilder, lootTableSource, provider) -> {
+            if (BuiltInLootTables.VILLAGE_PLAINS_HOUSE.equals(resourceKey) && lootTableSource.isBuiltin()) {
                 LootPool.Builder chestPool = LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(1))
                         .add(LootItem.lootTableItem(MoLoAPItems.APPLE_PIE).setWeight(97))
                         .add(LootItem.lootTableItem(Items.AIR).setWeight(903)); // Simulate Pumpkin Pie's 9.7% chest loot weight
                 tableBuilder.pool(chestPool.build());
-            } else if (BuiltInLootTables.FARMER_GIFT.equals(key) && source.isBuiltin()) {
+            } else if (BuiltInLootTables.FARMER_GIFT.equals(resourceKey) && lootTableSource.isBuiltin()) {
                 LootPool.Builder giftPool = LootPool.lootPool()
                         .setRolls(ConstantValue.exactly(1.0F))
                         .add(LootItem.lootTableItem(MoLoAPItems.APPLE_PIE));
